de90d649dd89e90e889dad9d4eb59f29dfe125d2
MrMarkus2 <markus.malek@hhu.de>
2016-07-25 17:57:00 +0200

    Add files via upload

---
+++ TestDrivenDevelopmentTrainer.java
@@ -0,0 +1,10 @@
+package main.java.tddt;
+
+import main.java.tddt.gui.GUIMain;
+
+
+public class TestDrivenDevelopmentTrainer {
+    public static void main(String[] args){
+        GUIMain.launchGUI(args);
+    }
+}
